757c6e885f00888886db10722d870ea3ea9d3a3a
Kunuut Kaali <kunuutkaali@gmail.com>
2023-01-24 19:29:05 +0100

    Decrypt part done

--- opg1.4/script.js
+++ opg1.4/script.js
@@ -2,68 +2,96 @@ const input = document.getElementById('cryptedInput');
 const shift = document.getElementById('shift');
 const result = document.getElementById('result');
 const decryptForm = document.getElementById('decryptForm');
-const alphabet = [
-    'a', 'b', 'c', 'd','e','f','g', 'h','i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','æ','ø','å'
+const decryptBtn = document.getElementById('decryptBtn');
+
+const alphabet = [ 
+    'a', 'b', 'c', 'd','e','f','g', 'h','i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 ];
-const toLetters = (shifted) =>{
-    let result = [];
-    for(let i = 0; i < shifted; i++){
-        for(let j = 0; j < aplhabet.length; j++){
-            if(shifted[i] == j){
-                result.push(aplhabet[j]);
-            }
-        }
+// Display used alphabet width numbers:
+const displayTable = document.getElementById('displayTable');
+alphabet.forEach((value, i) =>{
+    let letterDiv = document.createElement('div');
+    letterDiv.innerHTML = value + " <br> " + i;
+    displayTable.append(letterDiv);
+    let numberDiv = document.createElement('div');
+})
+
+// Objectives:
+// Encrypt
+// - Map indexes of input letters
+// - Shift Indexes with suplied int value
+// - Remap shifted indexed letters with alphabet
+// - Show result
+
+
+
+
+// Decrypt
+// - Map indexes of input letters - Function returns an indexed array
+const mapIndexes = (str) => {
+    let indexedArr = [];
+    for(let letter of str){
+        alphabet.forEach((value, i) => {
+            if(letter == value) indexedArr.push(i);
+        })
     }
-}
-const indexLetters = (str) => {
     return indexedArr;
 }
-const decript = (indexed, shift) =>{
-    let newIndexed = [];
-    for(index of indexed){
-        let newIndex = index-shift;
-        if(newIndex < 0){
-            if(shift > alphabet.length){
-                let newShift = shift%alphabet.length;
-                newIndex = alphabet.length-newShift;
-            }else{
-                newIndex = alphabet.length+newIndex;
-            }
+
+// - Shift Indexes with suplied int value
+const shiftIndexes = (arr, shift) => {
+    let shiftedIndexes = [];
+    for(let index of arr){
+        if(shift > alphabet.length){
+            shift = shift%alphabet.length;
+        }
+        if((index-shift) < 0){
+            shiftedIndexes.push(alphabet.length+(index-shift));
         }else{
-            console.log(newIndex);
+            shiftedIndexes.push(index-shift);
         }
-        console.log(newIndex);
     }
-    
-    // let result = "";
-    // let shifted = [];
-    // for(let i = 0; i < indexed.length; i++){
-    //     if(indexed[i]-shift < 0){
-    //         shifted.push(aplhabet.length % shift);
-    //     }else{
-    //         shifted.push(indexed[i]-shift)
-    //     }
-    // }
-    // console.log(shifted);
-    // toLetters(shifted)
+    return shiftedIndexes;
 }
-input.addEventListener('keyup', () =>{
-    let small = input.value.toLowerCase();
-    let inputArr = small.split(``);
-    let indexed = [];
-    inputArr.forEach((value)=>{
-        for(let i = 0; i < alphabet.length; i ++){
-            if(value == alphabet[i]){
-                indexed.push(i);
+
+// - Remap shifted indexed letters with alphabet
+const mapLetters = (arr) =>{
+    let resultArr = [];
+    for(let index of arr){
+        alphabet.forEach((value, i) =>{
+            if(index == i){
+                resultArr.push(value);
             }
-        }
-    });
-    decript(indexed, parseInt(shift.value))
-})
-shift.addEventListener('change', () =>{
-    console.log('shift changed');
-    console.log(parseInt(shift.value));
-})
+        })
+    }
+    return resultArr;
+}
+
+// Show result in textArea:
+const updateResult = (arr) =>{
+    let string = "";
+    for(let letter of arr){
+        string = string + letter;
+    }
+    result.innerText = string;
+}
+
+// Decrypt:
+const decrypt = () =>{
+    // IndexLetters
+    let indexArr = mapIndexes(input.value.toLowerCase());
+    // ShiftIndexes
+    let shifted = shiftIndexes(indexArr, parseInt(shift.value));
+    // RemapLetters
+    let resultArr = mapLetters(shifted);
+    // Show result
+    updateResult(resultArr);
+}
+
+// Eventhandlers:
+input.addEventListener('keyup', decrypt);
 decryptForm.addEventListener('submit', (e)=>{
     e.preventDefault();
+    decrypt();
 })
+shift.addEventListener('change', decrypt);

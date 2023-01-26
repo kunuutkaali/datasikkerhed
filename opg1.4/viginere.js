cf0f4fdb1cdcfe4d2b057ecb449dde6b1491bd8e
Kunuut Kaali <kunuutkaali@gmail.com>
2023-01-26 16:16:27 +0100

    Rewritten code

--- opg1.4/viginere.js
+++ opg1.4/viginere.js
@@ -1,202 +1,46 @@
-const displayTable = document.createElement('div');
-displayTable.setAttribute('id', 'displayTable');
-document.body.prepend(displayTable);
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-// Display used alphabet width numbers:
-alphabet.forEach((value, i) =>{
-    let letterDiv = document.createElement('div');
-    letterDiv.innerHTML = value + " <br> " + i;
-    displayTable.append(letterDiv);
-})
-
-
-// Decrypt defines
-const cryptedInput = document.getElementById('cryptedInput');
-const decryptKey = document.getElementById('decryptKey');
-const decryptResult = document.getElementById('decryptResult');
-const decryptForm = document.getElementById('decryptForm');
-const decryptBtn = document.getElementById('decryptBtn');
-
-// MapIndexes
-const mapIndexes = (str) => {
-    let indexedArr = [];
-    for(let letter of str){
-        if(letter == " "){
-            indexedArr.push(letter);
-        }else{
-            alphabet.forEach((value, i) => {
-                if(letter == value) indexedArr.push(i);
-            })
-        }
-    }
-    return indexedArr;
-}
-
-// - Shift Indexes with suplied int value
-const shiftIndexes = (arr, keySequence) => {
-    let shiftedIndexes = [];
-    let counter = 0;
-    let limit = keySequence.length-1;
-    for(let index of arr){
-        if(index == " "){
-            shiftedIndexes.push(' ');
-        }else{
-            let shift = keySequence[counter];
-            if(counter == limit){
-                counter = 0;
-            }else{
-                counter++;
-            }
-            if((index-shift) < 0){
-                shiftedIndexes.push(alphabet.length+(index-shift));
-            }else{
-                shiftedIndexes.push(index-shift);
-            }
-        }
-    }
-    return shiftedIndexes;
-}
-
-// - Remap shifted indexed letters with alphabet
-const mapLetters = (arr) =>{
-    let resultArr = [];
-    for(let index of arr){
-        if(index === " "){
-            resultArr.push(' ');
-        }else{
-            alphabet.forEach((value, i) =>{
-                if(index == i){
-                    resultArr.push(value);
-                }
-            })
-        }
-    }
-    return resultArr;
-}
-
-// Show result in textArea:
-const updateResult = (arr) =>{
-    let string = "";
-    for(let letter of arr){
-        string = string + letter;
-    }
-    decryptResult.innerText = string;
-}
-
-// Decrypt:
-const decrypt = () =>{ 
-    // MapKeySequence
-    let keySequence = mapIndexes(decryptKey.value.toLowerCase());
-
-    // IndexLetters
-    let indexArr = mapIndexes(cryptedInput.value.toLowerCase());
-
-    // ShiftIndexes
-    let shifted = shiftIndexes(indexArr, keySequence);
-
-    // // RemapLetters
-    let resultArr = mapLetters(shifted);
-
-    // // Show result
-    updateResult(resultArr);
-}
-
-// Decrypt Eventhandlers:
-cryptedInput.addEventListener('keyup', decrypt)
-decryptForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    decrypt();
-})
-decryptKey.addEventListener('keyup', decrypt)
-
-
-
-
-// Encrypt defines
-const encryptForm = document.getElementById('encryptForm');
-const encryptInput = document.getElementById('encryptInput');
-const encryptKey = document.getElementById('encryptKey');
-const encrypted = document.getElementById('encrypted');
-const encryptBtn = document.getElementById('encryptBtn');
-
-// - Shift Indexes with suplied int value
-const shiftWithKey = (arr, keySequence) => {
-    let shiftedIndexes = [];
-    let counter = 0;
-    let limit = keySequence.length-1;
-    for(let index of arr){
-        if(index === " "){
-            shiftedIndexes.push(' ');
-        }else{
-            let shift = keySequence[counter];
-            if(counter == limit){
-                counter = 0;
-            }else{
-                counter++;
-            }
-            if((index+shift) > alphabet.length){
-                shiftedIndexes.push((index+shift)-alphabet.length);
-            }else{
-                shiftedIndexes.push(index+shift);
-            }
-        }
-    }
-    return shiftedIndexes;
-}
-// Update encrypted
-const showEncrypted = (arr) =>{
-    let string = "";
-    for(let letter of arr){
-        string = string + letter;
-    }
-    encrypted.innerText = string;
-}
-
-// Encrypt
-const encrypt = () =>{
-    // - Map key index sequence - reuse function above
-    let keySequence = mapIndexes(encryptKey.value.toLowerCase())
-
-    // - Map indexes - reuse function above
-    let indexArr = mapIndexes(encryptInput.value.toLowerCase())
-
-    // - Shift Indexes with suplied key sequence
-    let shiftedIndexes = shiftWithKey(indexArr, keySequence);
-    
-    // - Remap shifted indexed letters with alphabet - reuse function above
-    let resultArr = mapLetters(shiftedIndexes);
-
-    // - Show result
-    showEncrypted(resultArr);
-}
-// Encript eventhandlers:
-encryptInput.addEventListener('keyup', encrypt)
-encryptKey.addEventListener('keyup', encrypt)
-encryptForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    encrypt();
-})
-
-const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const string = "MQT xv rx nhkobbbxewr pwk...";
-const key = "Kitten";
-const viginereDecipher = (str, key) =>{
+// Viginère De or encrypt function:
+const viginereCipher = (str, key, decrypt) =>{
     let counter = 0;
     const result = str.split('').map((letter) =>{
         if(alpha.includes(letter.toLowerCase())){
-            const charI = alpha.indexOf(letter.toLowerCase());
-            const keyIndex = alphabet.indexOf(key[counter % key.length]);
-            const newIndex = (charI - keyIndex + 26) % alpha.length;
-            if(letter === letter.toUpperCase()){
-                letter = alpha[newIndex].toUpperCase();
-            }else{
-                letter = alpha[newIndex];
-            }
+            const charIndex = alpha.indexOf(letter.toLowerCase());
+            const keyIndex = alpha.indexOf(key[counter % key.length]);
+            decrypt ? newIndex = (charIndex - keyIndex + alpha.length) % alpha.length : newIndex = (charIndex + keyIndex + 26) % alpha.length;
+            letter === letter.toUpperCase() ? letter = alpha[newIndex].toUpperCase() : letter = alpha[newIndex];
             counter++;
         }
         return letter;
     })
-    return result;
+    return result.join('');
 }
-const result = viginereDecipher(string, key.toLowerCase())
-console.log(result);
+const $ = document.querySelector.bind(document);
+const alpha = "abcdefghijklmnopqrstuvwxyz".split('');
+
+
+// Decrypt
+const decryptForm = document.getElementById('decryptForm');
+const decryptResult = document.getElementById('decryptResult');
+
+const decrypt = ()=>{    
+    const cryptedInput = $('#cryptedInput').value;
+    const decryptKey = $('#decryptKey').value.toLowerCase();
+    decryptResult.innerText = viginereCipher(cryptedInput, decryptKey, true)
+}
+decryptForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    decrypt();
+});
+
+
+// Encrypt
+const encryptForm = $('#encryptForm');
+const encryptedResult = document.getElementById('encryptedResult');
+
+const encrypt = () =>{
+    encryptedResult.innerText = viginereCipher($('#encryptInput').value, $('#encryptKey').value.toLowerCase(), 
+        false);
+}
+encryptForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    encrypt();
+})
